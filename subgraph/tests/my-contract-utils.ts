04f7b820935b9edfb54d5a2e81378170214a4512
Taylor <taylor.scheel@dltx.io>
2025-06-27 15:07:27 +1000

    chore: clean up before merging into mono repo

--- subgraph/tests/my-contract-utils.ts
+++
@@ -1,7 +0,0 @@
-import { ethereum } from '@graphprotocol/graph-ts';
-import { newMockEvent } from 'matchstick-as';
-
-// Simple mock event creator for testing
-export function createDummyEvent(): ethereum.Event {
-  return newMockEvent();
-}
